bb68ad4a36bf28c21c1e80c5cef28fd8480d8c36
Falynsky <kamil.falinski@gmail.com>
2019-05-17 00:20:50 +0200

    Initial commit

---
+++ src/TextComponentDecorator.java
@@ -0,0 +1,13 @@
+abstract class TextComponentDecorator extends TextComponent {
+
+    private TextComponent text;
+
+    TextComponentDecorator(TextComponent text) {
+        this.text = text;
+    }
+
+    @Override
+    public void show() {
+        text.show();
+    }
+}
